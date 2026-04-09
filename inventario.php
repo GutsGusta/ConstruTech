98f4305cd691d03fe8d3f526b9fb88cfcecf5875
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-04-09 14:43:28 -0300

    hi

---
+++ inventario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="Viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style/style.css">
+	<link rel="icon" type="image/x-icon" href="imagens/Logo.png">
+	<title>ConstruTech</title>
+</head>
+<body>
+	<main>
+	</main>
+</body>
+
+
