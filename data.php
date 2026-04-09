465bb599d87e7706f4fd51a4cc4b892cf70ad74a
GutsGusta <gustavonovaes623@gmail.com>
2026-04-09 15:09:44 -0300

    coloquei o esqueleto dos produtos de do data

---
+++ data.php
@@ -0,0 +1,75 @@
+<?php
+
+$categorias = [
+    'bruto' => 'Bruto'
+    'ferramentas'=> 'Ferramenta'
+    'acabamento' => 'Acabamento'
+];
+
+$produtos_base = [
+    [
+        'id' => 1,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 2,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 3,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 4,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 5,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 6,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 7,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ],
+    [
+        'id' => 8,
+        'nome' => 'Cimento',
+        'categoria' => 'Bruto',
+        'preco' => 'R$35,00',
+        'estoque' => 30,
+        'investimento' => 600
+    ]
+];
+?>
