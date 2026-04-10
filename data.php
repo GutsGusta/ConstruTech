e3004d4475872832d4a1651808e7782196fca0c4
GutsGusta <gustavonovaes623@gmail.com>
2026-04-10 16:15:10 -0300

    Adição dos produtos no data

--- data.php
+++ data.php
@@ -13,71 +13,79 @@ $produtos_base = [
         'categoria' => 'Bruto',
         'preco' => 'R$35,00',
         'estoque' => 30,
-        'investimento' => 600,
-        'imagem' => 'imagens/'
+        'investimento' => 'R$2000,00',
+        'retorno' => 'R$3500,00',
+        'imagem' => 'imagens/Cimento50kg.svg'
     ],
     [
         'id' => 2,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Colher de Pedreiro',
+        'categoria' => 'Ferramenta',
+        'preco' => 'R$26,00',
+        'estoque' => 40,
+        'investimento' => 'R$1000,00',
+        'retorno' => 'R$2600,00',
+        'imagem' => 'imagens/ColherPedreiro.svg'
     ],
     [
         'id' => 3,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Porcelanato Ortiz Cinza Acetinado Retificado 120x120cm',
+        'categoria' => 'Acabamento',
+        'preco' => 'R$42,00',
+        'estoque' => 60,
+        'investimento' => 'R$2000,00',
+        'retorno' => 'R$4200,00',
+        'imagem' => 'imagens/PorcelanatoOrtiz.svg'
     ],
     [
         'id' => 4,
-        'nome' => 'Cimento',
+        'nome' => 'Argamassa 20kg',
         'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'preco' => 'R$25,00',
+        'estoque' => 67,
+        'investimento' => 'R$800,00',
+        'retorno' => 'R$2500,00',
+        'imagem' => 'imagens/Argamassa.svg'
     ],
     [
         'id' => 5,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Parafusadeira e Furadeira - Makita',
+        'categoria' => 'Ferramenta',
+        'preco' => 'R$550,00',
+        'estoque' => 16,
+        'investimento' => 'R$25000,00',
+        'retorno' => 'R$55000,00',
+        'imagem' => 'imagens/Parafusadeira.svg'
     ],
     [
         'id' => 6,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Nível a Laser Bosch',
+        'categoria' => 'Ferramenta',
+        'preco' => 'R$670,00',
+        'estoque' => 24,
+        'investimento' => 'R$28000,00',
+        'retorno' => 'R$67000,00',
+        'imagem' => 'imagens/NivelaLaser.svg'
     ],
     [
         'id' => 7,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Rodapé Branco 10cm',
+        'categoria' => 'Acabamento',
+        'preco' => 'R$20,00',
+        'estoque' => 441,
+        'investimento' => 'R$700,00',
+        'retorno' => 'R$2000,00',
+        'imagem' => 'imagens/Rodape.svg'
     ],
     [
         'id' => 8,
-        'nome' => 'Cimento',
-        'categoria' => 'Bruto',
-        'preco' => 'R$35,00',
-        'estoque' => 30,
-        'investimento' => 600,
-        'imagem'
+        'nome' => 'Revestimento Ripado 30x90',
+        'categoria' => 'Acabamento',
+        'preco' => 'R$135,00',
+        'estoque' => 777,
+        'investimento' => 'R$8000,00',
+        'retorno' => 'R$13500,00',
+        'imagem' => 'imagens/Revestimento.svg'
     ]
 ];
 ?>
