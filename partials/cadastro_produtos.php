b4d6ade62ece5a047cdf19d005262eba5ef12faa
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-04-10 14:30:25 -0300

    Kept developing the website

---
+++ partials/cadastro_produtos.php
@@ -0,0 +1,5 @@
+$nome = htmlspecialchars($_POST['nome']);
+$preco = htmlspecialchars($_POST['preco']);
+$investimento = htmlspecialchars($_POST['investimento']);
+$estoque = htmlspecialchars($_POST['estoque'];
+$categoria = htmlspecialchars($_POST['categoria']);
