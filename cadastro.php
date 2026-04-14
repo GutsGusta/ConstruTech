28bfd558af5cd0f600cce8b9c6522fc98f3b9c47
GutsGusta <gustavonovaes623@gmail.com>
2026-04-14 14:56:49 -0300

    Estilização do cadastro de produtos

--- cadastro.php
+++ cadastro.php
@@ -32,28 +32,31 @@
 		require_once 'partials/header.php';
 	?>
 	<main>
-		<form action="index.php" method="POST">
+		<div class="main-form">
 			<h1>Cadastro de produto</h1>
-			<label for="nome">Nome</label>
-			<br>
-			<input type="text" maxlength="200" id="nome" name="nome" required><br>
-			<label for="preco">Preço</label>
-			<br>
-			<input type="number" maxlength="20" id="preco" name="preco" min="0" required><br>
-			<label for="investimento">Investimento</label>
-			<br>
-			<input type="number" maxlength="20" id="investimento" name="investimento" min="0" required><br>
-			<label for="estoque">Estoque</label>
-			<br>
-			<input type="number" id="estoque" name="estoque" maxlength="20" min="0" required><br>
-			<label for="categoria">Categoria</label>
-			<br>
-			<select name=categoria id=categoria required>
-				<option value="bruto">Bruto</option>
-				<option value="ferramenta">Ferramenta</option>
-				<option value="acabamento">Acabamento</option>	
-			</select><br><br>
-			<button type="submit">Enviar</button>				
-		</form>
+			<form action="index.php" method="POST" class="formulario">
+				
+				<label for="nome">Nome</label>
+				<br>
+				<input type="text" maxlength="200" id="nome" name="nome" required><br>
+				<label for="preco">Preço</label>
+				<br>
+				<input type="number" maxlength="20" id="preco" name="preco" min="0" required><br>
+				<label for="investimento">Investimento</label>
+				<br>
+				<input type="number" maxlength="20" id="investimento" name="investimento" min="0" required><br>
+				<label for="estoque">Estoque</label>
+				<br>
+				<input type="number" id="estoque" name="estoque" maxlength="20" min="0" required><br>
+				<label for="categoria">Categoria</label>
+				<br>
+				<select name=categoria id=categoria required>
+					<option value="bruto">Bruto</option>
+					<option value="ferramenta">Ferramenta</option>
+					<option value="acabamento">Acabamento</option>	
+				</select><br><br>
+				<button type="submit" class="botao-enviar">Enviar</button>				
+			</form>
+		</div>
 	</main> 
 </body>
--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
     <header class="cabecalho">
         <img src="imagens/Logo.png">
         <nav>
-            <a href="index.html" class="botaoCabecalho">Produtos</a>
+            <a href="index.php" class="botaoCabecalho">Produtos</a>
             <a href="cadastro.php" class="botaoCabecalho">Cadastro de Produtos</a>
             <a href="inventario.php" class="botaoCabecalho">Inventário</a>
         </nav>
